875de57831e2363a43f225587fc76cd0ea9cf020
Alexandr <alexandr.kopyletz@gmail.com>
2024-06-24 23:45:33 +0300

    24/06-2

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items.js';
+// import { galleryItems } from './1-gallery.js';
 
 const images = [
       {
